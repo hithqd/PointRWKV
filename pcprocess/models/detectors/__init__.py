667149998f750308d63c9a32ea0d43c5484fa6d5
Zijiyingcai <37433515+hithqd@users.noreply.github.com>
2024-12-31 14:56:26 +0800

    Add files via upload

---
+++ pcprocess/models/detectors/__init__.py
@@ -0,0 +1,35 @@
+from .detector3d_template import Detector3DTemplate
+from .point_rcnn import PointRCNN
+from .pointpillar import PointPillar
+from .pv_rcnn import PVRCNN
+from .second_net import SECONDNet
+from .ours import PointTRCNN
+from .ours_3d import PointTRCNN3D
+from .second_net_iou import SECONDNetIoU
+from .caddn import CaDDN
+from .voxel_rcnn import VoxelRCNN
+from .centerpoint import CenterPoint
+from .pv_rcnn_plusplus import PVRCNNPlusPlus
+
+__all__ = {
+    'Detector3DTemplate': Detector3DTemplate,
+    'SECONDNet': SECONDNet,
+    'PVRCNN': PVRCNN,
+    'PointPillar': PointPillar,
+    'PointRCNN': PointRCNN,
+    "Ours": PointTRCNN,
+    "Ours_3d": PointTRCNN3D,
+    'SECONDNetIoU': SECONDNetIoU,
+    'CaDDN': CaDDN,
+    'VoxelRCNN': VoxelRCNN,
+    'CenterPoint': CenterPoint,
+    'PVRCNNPlusPlus': PVRCNNPlusPlus
+}
+
+
+def build_detector(model_cfg, num_class, dataset):
+    model = __all__[model_cfg.NAME](
+        model_cfg=model_cfg, num_class=num_class, dataset=dataset
+    )
+
+    return model
