603148853d39fb1210607fe4f148eecd78e7406e
Zijiyingcai <37433515+hithqd@users.noreply.github.com>
2026-03-20 10:59:19 +0800

    Update dist_test.sh

--- scripts/dist_test.sh
+++ scripts/dist_test.sh
@@ -1,8 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# PointRWKV distributed testing script
+# Usage: bash scripts/dist_test.sh <NUM_GPUS> <SCRIPT> [OTHER_ARGS]
+# Example: bash scripts/dist_test.sh 1 main_cls.py --config cfgs/cls_modelnet40.yaml --test --ckpt /path/to/ckpt --vote
 
-set -x
-NGPUS=$1
-PY_ARGS=${@:2}
+NUM_GPUS=$1
+shift
+SCRIPT=$1
+shift
 
-python -m torch.distributed.launch --nproc_per_node=${NGPUS} test.py --launcher pytorch ${PY_ARGS}
-
+torchrun \
+    --nproc_per_node=${NUM_GPUS} \
+    --master_port=$((RANDOM + 10000)) \
+    ${SCRIPT} \
+    --launcher pytorch \
+    --test \
+    "$@"
