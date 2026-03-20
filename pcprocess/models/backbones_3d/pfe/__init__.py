239b7fe8e233158ece01d876bca2273bc7f0b14a
Zijiyingcai <37433515+hithqd@users.noreply.github.com>
2026-03-20 10:54:29 +0800

    Delete pcprocess directory

--- pcprocess/models/backbones_3d/pfe/__init__.py
+++
@@ -1,7 +0,0 @@
-from .voxel_set_abstraction import VoxelSetAbstraction, \
-                                VoxelSetAbstractionTransFusionv5
-
-__all__ = {
-    'VoxelSetAbstraction': VoxelSetAbstraction,
-    'VoxelSetAbstractionTransFusionv5': VoxelSetAbstractionTransFusionv5
-}
