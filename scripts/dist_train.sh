d2cd580751af67c276c385d9cb8146e7c7094fb7
Zijiyingcai <37433515+hithqd@users.noreply.github.com>
2026-03-20 10:59:00 +0800

    Update dist_train.sh

--- scripts/dist_train.sh
+++ scripts/dist_train.sh
@@ -1,8 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# PointRWKV distributed training script
+# Usage: bash scripts/dist_train.sh <NUM_GPUS> <SCRIPT> [OTHER_ARGS]
+# Example: bash scripts/dist_train.sh 4 main_cls.py --config cfgs/cls_modelnet40.yaml --exp_name cls_mn40
 
-set -x
-NGPUS=$1
-PY_ARGS=${@:2}
+NUM_GPUS=$1
+shift
+SCRIPT=$1
+shift
 
-python -m torch.distributed.launch --nproc_per_node=${NGPUS} train.py --launcher pytorch ${PY_ARGS}
-
+torchrun \
+    --nproc_per_node=${NUM_GPUS} \
+    --master_port=$((RANDOM + 10000)) \
+    ${SCRIPT} \
+    --launcher pytorch \
+    "$@"
